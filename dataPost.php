d94821c2aeb3d6e507d877d3b0fcffea312881e2
FloGro <grogne_f@etna-alternance.net>
2014-04-02 10:21:27 +0200

    Create dataPost.php
    Fonctions liés à la manipulation de posts sur la base de données.

---
+++ dataPost.php
@@ -0,0 +1,10 @@
+<?php
+require_once "dataConnect.php";
+
+//function addPost($category, $author, $content);
+//function delPost($id);
+//function showPost($id);
+//function showAllPost();
+//function showMultiplePost($field, $value);
+//function getPostID($author, $date);
+?>
