9dd40d7842787480678cd40c50b7af60f5860fda
FloGro <grogne_f@etna-alternance.net>
2014-04-02 10:22:38 +0200

    Create dataMessages.php
    Fonctions liées à la manipulation de messages sur la base de données.

---
+++ dataMessages.php
@@ -0,0 +1,10 @@
+<?php
+require_once "dataConnect.php";
+
+//function addMessage($content, $author);
+//function delMessage($id);
+//function getMessageID($author, $date);
+//function showMessage($id);
+//function showMessagesPerAuthor($authorID);
+//function getMessageContent($id);
+?>
